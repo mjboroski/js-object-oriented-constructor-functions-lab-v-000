8f4475aed236a99b68e82709b0315ce0f50ad3e8
Michael Boroski <mjboroski@gmail.com>
2018-07-09 07:19:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function Scooter(year, color, model){
+(function Scooter(year, color, model){
   this.year = year
   this.color = color
   this.model = model
-    
-}
+
+})
 
 function Driver(name, age, experience ){
   this.name = name
